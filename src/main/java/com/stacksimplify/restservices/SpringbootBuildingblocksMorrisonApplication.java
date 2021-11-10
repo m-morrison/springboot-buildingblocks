f72033424781fda61f54c8ee6bb681b76c50193c
m-morrison <mmorrison585@gmail.com>
2021-11-10 00:40:01 -0500

    i18n first commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksMorrisonApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksMorrisonApplication.java
@@ -1,13 +1,32 @@
 package com.stacksimplify.restservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class SpringbootBuildingblocksMorrisonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksMorrisonApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
