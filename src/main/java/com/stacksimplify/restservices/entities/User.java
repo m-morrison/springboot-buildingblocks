2dad6666f7ea427de4dcd1307842ba43baa68cd6
m-morrison <mmorrison585@gmail.com>
2021-11-10 16:20:35 -0500

    MappingJacksonValue - First Commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -13,12 +13,14 @@ import javax.validation.constraints.Size;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "user")
-@JsonIgnoreProperties({"firstname", "lastname"})
+//@JsonIgnoreProperties({"firstname", "lastname"}) -- Static Filtering JsonIgnore
+@JsonFilter(value = "userFilter")
 public class User extends RepresentationModel<User> {
 	
 	@Id
@@ -43,7 +45,7 @@ public class User extends RepresentationModel<User> {
 	private String role;
 	
 	@Column(name = "SSN", length = 50, nullable = false, unique = true)
-	@JsonIgnore
+	//@JsonIgnore -- Static Filtering
 	private String ssn;
 	
 	@OneToMany(mappedBy = "user")
