3597e57271df19f8cf85934768891e3f9ced7269
m-morrison <mmorrison585@gmail.com>
2021-11-13 14:59:20 -0500

    MapStruct - First Commit

---
+++ src/main/java/com/stacksimplify/restservices/mappers/UserMapper.java
@@ -0,0 +1,23 @@
+package com.stacksimplify.restservices.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.stacksimplify.restservices.dtos.UserMsDto;
+import com.stacksimplify.restservices.entities.User;
+
+@Mapper(componentModel = "Spring")
+public interface UserMapper {
+
+	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+	
+	//User to User DTO
+	@Mapping(source="email", target="emailAddress")
+	UserMsDto userToUserMsDto(User user);
+	
+	//List<User> to List<UserMsDto>
+	List<UserMsDto> usersToUserDtos(List<User> users);
+}
